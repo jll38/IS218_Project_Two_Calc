4f2b6540bacd03a3592d629c81ccd140a43f287f
Julian <jll38@njit.edu>
2022-03-28 18:18:08 -0400

    Page

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -35,7 +35,31 @@ def test_request_page4(client):
     assert response.status_code == 200
     assert b"CI/CD" in response.data
 
-def test_request_page_not_found(client):
+def test_request_page5(client):
     """This makes the index page"""
     response = client.get("/page5")
+    assert response.status_code == 200
+    assert b"Pylint" in response.data
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"AAA testing" in response.data
+
+def test_request_page7(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"OOPs" in response.data
+
+def test_request_page8(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"SOLID" in response.data
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page9")
     assert response.status_code == 404
