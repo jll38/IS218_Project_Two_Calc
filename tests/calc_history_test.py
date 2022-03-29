00518d440beccc54c700af34c767798fda553d99
Julian <jll38@njit.edu>
2022-03-29 00:00:32 -0400

    OOP

--- tests/calc_history_test.py
+++
@@ -1,9 +0,0 @@
-"""Testing the Calculator History"""
-from calculator.history import Calculations
-
-def test_calculation_is_instance():
-    """Testing the Calculator"""
-    calculation = Calculations()
-    assert isinstance(calculation, Calculations)
-
-
